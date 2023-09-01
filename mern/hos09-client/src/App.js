560c97736f93d29bc6a956dd768edc25765ee960
sharpc42 <chris.m.sharp@gmail.com>
2023-08-31 18:37:14 -0700

    Submit for Mod 9 - Chris Sharp

--- mern/hos09-client/src/App.js
+++ mern/hos09-client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Navbar from './components/navbar'
 import RecordList from './components/recordList'
-import Edit from './comonent/edit'
+import Edit from './components/edit'
 import Create from './/components/create'
 
 const App = () => {
